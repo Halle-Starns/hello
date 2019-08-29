353cdb5a0bb23b06f139401446487fad5f3053c3
Halle Starns <halle@Halles-MacBook-Pro.local>
2019-08-29 18:57:15 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+  * Author: Halle Starns, Parul Aggarwal, Claire Sorenson, and Sara Qudus
+  * Date: 2019/08/29
+  *
+  * A simple hello world program in C
+  *
+  */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Halle Starns, Parul Aggarwal, Claire Sorenson, and Sara Qudus\n");
+
+  return 0;
+}
